d232000b6c48ea7857533e4adb1caed4643f558d
Defixenas <defixass@gmail.com>
2023-05-12 13:20:48 +0300

    Added password hashing and user creation endpoint.

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from .database import Session
-from . import model, schema
+from . import model, schema, utils
 from fastapi import Depends, FastAPI, HTTPException, status
 
 
@@ -20,9 +20,10 @@ def get_db():
 
 app = FastAPI()
 
-# ---------------------------------------------------------------- ENDPOINTS START HERE
+# ---------------------------------------------------------------- POST PATHS START HERE
 
 
+# Get all posts
 @app.get("/posts", response_model=list[schema.Post])
 def get_all_posts(db: Session = Depends(get_db)):
     posts = db.query(model.Post).all()
@@ -32,11 +33,11 @@ def get_all_posts(db: Session = Depends(get_db)):
 # Create a post and add it to the posts table
 @app.post("/posts", status_code=status.HTTP_201_CREATED, response_model=schema.Post)
 def create_post(post: schema.PostBase, db: Session = Depends(get_db)):
-    new_post = model.Post(**post.dict())
-    db.add(new_post)
+    new_user = model.Post(**post.dict())
+    db.add(new_user)
     db.commit()
-    db.refresh(new_post)
-    return new_post
+    db.refresh(new_user)
+    return new_user
 
 
 # Get a specific post by id from the posts table.
@@ -74,3 +75,14 @@ def update_post(
         db.refresh(post)
         return post
     raise HTTPException(status_code=404, detail="Post not found")
+
+
+# -------------- user
+@app.post("/users", response_model=schema.UserOut)
+def create_user(user: schema.UserCreate, db: Session = Depends(get_db)):
+    hashed_password = utils.hash_password(user.password)
+    new_user = model.User(email=user.email, password=hashed_password)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
--- app/schema.py
+++ app/schema.py
@@ -1,6 +1,6 @@
 # Schema for fastapi
 from datetime import datetime
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 
 class PostBase(BaseModel):
@@ -15,6 +15,28 @@ class CreatePost(PostBase):
 
 class Post(PostBase):
     created_at: datetime
+
+    class Config:
+        orm_mode = True
+
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+
+    # Providing an example for my schema
+    class Config:
+        schema_extra = {
+            "example": {
+                "email": "john.doe@example.com",
+                "password": "secure_password",
+            }
+        }
+
+
+class UserOut(BaseModel):
+    id: int
+    email: EmailStr
 
     class Config:
         orm_mode = True
