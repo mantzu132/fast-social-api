3142a0aec7fe093c02a5acfd1a549b57ea3c41d2
Defixenas <defixass@gmail.com>
2023-05-27 17:46:46 +0300

    enabling CORS for later

--- app/main.py
+++ app/main.py
@@ -1,9 +1,23 @@
 from fastapi import FastAPI
 from .routers import post, user, auth, vote
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
+# ---------- ENABLING CORS
+origins = [
+    "http://localhost:3000",
+    "http://yourdomain.com",
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+# -----------------
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
